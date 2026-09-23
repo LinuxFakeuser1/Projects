d6f1e1c1f3f21158d613025fd092fe5be85a82c1
Alan <2630292@upv.edu.mx>
2026-09-23 00:12:36 -0600

    aqui se habla sobre los strings, normas al usarlos y metodos como upper, lower y title

---
+++ programming_meths_2/srs/Understanding_Strings.py
@@ -0,0 +1,52 @@
+# STRINGS
+
+"""
+un string es de manera sencilla es una serie de caracteres.
+En Python, todo lo que se encuentre entre comillas simples '' 
+o dentro de comillas dobles "" esconsiderado un string
+   Ejemplo:
+
+   "Esto es un string"
+   'esto también es un string' 
+   'le dije a mi amigo, "python es mi lenguage favorito"'
+   "El lenguge 'Python' Lleva ese nombre por Monty Python, no por la serpiente"
+   "Esto no es un string'
+   'Tampoco esto
+   Ni esto"
+   Tampoco ninguno de estos:'
+       "'
+       '"
+       '
+        '
+
+"""
+name = "alan aNgUIano AYALa"
+print (name)
+
+print (name.title())
+print (name)
+
+name = print(name.title())
+print (name)
+
+"""
+Un método es una acción que Python 
+puede realizar sobre una variable.
+
+El punto . después de una variable
+seguido por el nombre del método en este caso title() 
+dice que tiene que ejecutar el metodo title() 
+de la variable name.
+
+Todos los métodos van seguidos de parentesis 
+porque en ocasiónes necesitan información 
+adicional para trabajar. En esta ocasion 
+el método title() no reqquiere información 
+adicional para Ejecutarse.
+"""
+# OTROS MÉTODOS
+print("----------")
+print (name)
+name = "Alan aNguiano AYla"
+print(name.upper())
+print(name.lower())
