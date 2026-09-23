049fd19a00fb68ec19115549d6dd193b02f60b4d
Alan <2630292@upv.edu.mx>
2026-09-22 23:17:01 -0600

    Understandable_variables: el profe nos eseño como utilizar las variables dentro de python y sus funciones mas basicas

---
+++ programming_meths_2/srs/Understandable_variables.py
@@ -0,0 +1,44 @@
+# let's try to use variables
+message = "this is my first program :), I am very happy"
+another_message = "Really, so so so happy C:"
+
+print(message)
+print(another_message)
+
+print(message, another_message, "Six seven")
+print(message, another_message, "Six seven")
+
+message = "hi boys & girls"
+print (message)
+
+"""
+ mi nombre es Alan Anguiano
+ pero la raza me dice pipila
+ Nadie tiene tiempo de jugar roblox conmigo
+ Arriba la grasa
+
+ Reglas generales para nombrar variables en python
+
+ Las variables en python deben nombrarse solo con:
+
+ - palabras en ingles
+ - letras, numeros y guion bajo"
+ - Deben comenzar cin una letra o guion bajo, pero no con numeros
+ - NOutilizar espacios para separar palabras
+ - NO utilizar palabras reservadas por Python o el nombre del archivo como nombre del archivo como variables
+ - los nombres deben ser cortos pero descriptivos
+ - letras minusculas ¡¡¡ solo por ahora !!!
+Ejemplo:
+    correcto: message_1, _message_1
+    incorrecto: 1message_1
+"""
+message = "hola amigo python"
+print(message)
+"""   
+    El traceback es el registro en donde el interprete
+    tuvo problemas al intentar ejeccutar el codigo
+ 
+    NameError: Sucede cuando olvidamos establecer el vlor de una variable antes de utilizarla
+    o cometemos un error ortpgrafico al ingresar el nombre de una variable (Typos)
+    
+"""
