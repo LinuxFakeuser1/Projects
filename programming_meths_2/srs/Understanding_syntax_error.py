d8491f9921bc4c5e85cc57ccd231fd64fce760c8
Alan <2630292@upv.edu.mx>
2026-09-23 08:49:43 -0600

    En la clase de hoy, aparte de chismear  con el profe charly vimos el syntax error

---
+++ programming_meths_2/srs/Understanding_syntax_error.py
@@ -0,0 +1,15 @@
+# Eliminación de espacios en blanco
+programming_language = "     |python     "
+print(programming_language)
+print("fecto del lstrip")
+print(programming_language.lstrip())
+print("efecto del rstrip")
+print(programming_language.rstrip())
+print("efecto del strip")
+print(programming_language.strip())
+
+# Error de syntaxis
+message = "una fortaleza de python es su comunidad"
+print(message)
+message = "una fortaleza de 'python' es su comunidad"
+print(message)
