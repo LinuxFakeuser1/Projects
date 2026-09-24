9cd0da8ca87b6d128f26806a9b30dbbe87f3d227
Alan <2630292@upv.edu.mx>
2026-09-24 08:22:44 -0600

    El porfe nos explicó el tipo de variables numericas que hay y sus diferencias entre otros tipos de variables

---
+++ programming_meths_2/srs/Understanding_numbers.py
@@ -0,0 +1,16 @@
+# Numbers
+# Enteros + Integers
+"""
+     Los numeros enteros los podemos
+    sumar (+), restar (-), multiplicar(*) y 
+    dividir(/)
+"""
+print(2+4)
+print(2-4)
+print(2*4)
+print(2/4)
+
+number_1 = 5
+number_2 = 10
+number_3 = (number_1+number_2)
+number_4 = (number_1/number_2)
